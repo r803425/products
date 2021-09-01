3c532a057b46e1c6c924f63fa33dfa462e658d70
Ryan <r803425@gmail.com>
2021-09-01 17:20:11 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,16 +1,33 @@
+import os #operating system
 products = []
 
+
+# 讀取檔案檢查檔案有沒有已經存在 #import os
+if os.path.isfile('products.csv'): #使用os的path檢查路徑功能的isfile功能:有沒有存在
+    print('GOOD有找到檔案')
+    with open('products.csv', 'r', encoding='utf-8') as f: #將下面讀取檔案功能全部移上來
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案哦!!!')
+    
+
 # 讀取檔案
-with open('products.csv', 'r', encoding='utf-8') as f: #要用utf-8編碼讀取檔案
-    for line in f:
-        if '商品,價格' in line: #if line字串裡面，有 商品,價格，就執行continue
-            continue #continue為強制執行下一回圈，不繼續做下面的code (continue / break只能寫在迴圈裡)
-        name, price = line.strip().split(',') #strip切掉換行\n 跟 前後空格 #split分割字串中的,符號
-        # name = s[0] #此兩行可以直接寫在上一行裡面，用,切割成幾塊，分別對應幾個變數
-        # price = s[1]
-        products.append([name, price])
-        #print(s) #將字串用split切割完後，變成清單
-print(products)
+# with open('products.csv', 'r', encoding='utf-8') as f: #要用utf-8編碼讀取檔案
+#     for line in f:
+#         if '商品,價格' in line: #if line字串裡面，有 商品,價格，就執行continue
+#             continue #continue為強制執行下一回圈，不繼續做下面的code (continue / break只能寫在迴圈裡)
+#         name, price = line.strip().split(',') #strip切掉換行\n 跟 前後空格 #split分割字串中的,符號
+#         # name = s[0] #此兩行可以直接寫在上一行裡面，用,切割成幾塊，分別對應幾個變數
+#         # price = s[1]
+#         products.append([name, price])
+#         #print(s) #將字串用split切割完後，變成清單
+# print(products)
+
 
 # 讓使用者輸入
 while True:
@@ -18,14 +35,11 @@ while True:
     if name == 'q':
         break
     #products.append(name)
-
     price = input('請輸入"價格": ')
-    
     # p = [] #變成二維清單，先假設有一個小清單，之後再放到大清單裡面
     # p.append(name)
     # p.append(price)
     # p = [name, price] #上面3行可以改寫成這一行
-
     # products.append(p) #二維清單，小清單放到大清單裡
     products.append([name, price]) #進階改寫成這樣
 #print(products) #本來這是一維清單，只放了products名稱
@@ -35,9 +49,11 @@ print(products) #變成二維清單，清單中再放入清單，大清單裝小
 # print(products[1][0])
 # print(products[1][1])
 
+
 # 印出所有購買紀錄
 for p in products: #for迴圈，把清單中的小清單，一次一次取出來
     print(p[0], '的價格是', p[1]) #小清單裡面的[0][1]的位置
+
 
 # 寫入檔案
 # 'abc' + '123' = 'abc123'
